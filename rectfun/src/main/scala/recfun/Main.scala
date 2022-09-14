fb4b28c8376e0aa2a7a99d138bda1d87eafdb1ef
gloveman <gloveman@postech.ac.kr>
2022-09-14 19:28:45 +0900

    Complete Pascal Triangle

--- rectfun/src/main/scala/recfun/Main.scala
+++ rectfun/src/main/scala/recfun/Main.scala
@@ -3,26 +3,45 @@ import common._
 
 object Main {
   def main(args: Array[String]) {
+    /*
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+     */
+    println(balance("(if (zero? x) max (/ 1 x))".toList))
   }
+
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(r==0||c==0)
+      1
+    else if(r==c)
+      1
+    else
+      pascal(c,r-1)+pascal(c-1,r-1)
+
+
+
+  }
+
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    false
+
+
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = 3
 }
